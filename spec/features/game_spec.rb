52f2ffd7a980cd3a6e428c9c4f94fed7e053a4f8
Anna <anna.voitik@gmail.com>
2020-05-13 13:21:00 +0100

    Skinny controllers

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,6 +1,6 @@
 require 'game'
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
 let(:player_1) { double :player }
 let(:player_2) { double :player }
 
@@ -10,4 +10,16 @@ describe '#attack' do
     game.attack(player_2)
   end
 end
+
+describe '#player 1' do
+  it 'accepts player 1' do
+    expect(game.player_1).to eq player_1
+  end
+end
+
+describe '#player 2' do
+  it 'accepts player 2' do
+    expect(game.player_2).to eq player_2
+  end
+end 
 end
