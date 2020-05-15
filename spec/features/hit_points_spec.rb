a25f1b95d8010c89c80e8a2da7454dba13941e2d
Anna <anna.voitik@gmail.com>
2020-05-15 14:07:39 +0100

    Player 1 and Player 2 can see their hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -4,3 +4,10 @@ feature 'Show hit points of player 2' do
     expect(page).to have_content "Mittens: 60HP"
   end
 end
+
+feature 'view hit points' do
+  scenario 'player 1 can see their own hit points' do
+    sign_in_and_play
+    expect(page).to have_content "Dave: 60HP"
+  end
+end 
